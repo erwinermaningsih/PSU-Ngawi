790c93a1e6ec4bed643c8b1a2456f9b98e62d439
erwinermaningsih <erwinermaningsih.perkim@gmail.com>
2026-02-13 15:22:02 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,40 @@
 // ===============================
-// MAP
+// INISIALISASI MAP
 // ===============================
-var map = L.map('map').setView([-7.4,111.4],13);
+var map = L.map('map').setView([-7.4, 111.4], 13);
 
 // ===============================
 // BASEMAP
 // ===============================
 var osm = L.tileLayer(
 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-{ maxZoom:19, attribution:'© OpenStreetMap' }
+{
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+}
 ).addTo(map);
 
 // ===============================
 // STYLE
 // ===============================
-function styleJalan(){
-    return { color:"red", weight:6 };
+function styleJalan() {
+    return {
+        color: "#e10600",
+        weight: 6
+    };
 }
 
-function styleKelurahan(){
+function styleKelurahan() {
     return {
-        color:"#0047AB",
-        weight:2,
-        fillColor:"#00AEEF",
-        fillOpacity:0.2
+        color: "#0047AB",
+        weight: 2,
+        fillColor: "#00AEEF",
+        fillOpacity: 0.15
     };
 }
 
 // ===============================
-// VARIABLE LAYER
+// VARIABLE GLOBAL LAYER
 // ===============================
 var layerJalan;
 var layerKelurahan;
@@ -36,34 +42,39 @@ var layerKelurahan;
 // ===============================
 // LOAD JALAN
 // ===============================
-fetch("Jalan.geojson")
-.then(res => res.json())
+fetch("./Jalan.geojson")
+.then(res => {
+    if (!res.ok) throw new Error("Jalan.geojson tidak ditemukan");
+    return res.json();
+})
 .then(data => {
 
-    layerJalan = L.geoJSON(data,{
+    layerJalan = L.geoJSON(data, {
 
-        style:styleJalan,
+        style: styleJalan,
 
-        onEachFeature:function(feature, layer){
+        onEachFeature: function (feature, layer) {
 
             var p = feature.properties;
             var nama = p["Fungsi Jalan"] || "Ruas Jalan";
 
-            layer.bindTooltip(nama,{
-                permanent:true,
-                direction:"center",
-                className:"label-jalan"
+            // LABEL TENGAH
+            layer.bindTooltip(nama, {
+                permanent: true,
+                direction: "center",
+                className: "label-jalan"
             });
 
-            layer.on("click",function(){
+            // KLIK JALAN
+            layer.on("click", function () {
 
                 layer.unbindTooltip();
 
                 document.getElementById("info-content").innerHTML = `
-                <b>Nama Jalan :</b> ${nama}<br>
-                <b>Kondisi :</b> ${p["Kondisi Jalan "] || "-"}<br>
-                <b>Tipe Perkerasan :</b> ${p["Tipe Perkerasan"] || "-"}<br>
-                <b>Panjang :</b> ${p["Panjang Jln"] || 0} meter
+                    <b>Nama Jalan :</b> ${nama}<br>
+                    <b>Kondisi :</b> ${p["Kondisi Jalan "] || "-"}<br>
+                    <b>Tipe Perkerasan :</b> ${p["Tipe Perkerasan"] || "-"}<br>
+                    <b>Panjang :</b> ${p["Panjang Jln"] || 0} meter
                 `;
 
                 document.getElementById("info-panel").classList.remove("hidden");
@@ -71,65 +82,105 @@ fetch("Jalan.geojson")
                 window.layerAktif = layer;
                 window.namaTooltip = nama;
             });
-
         }
 
     }).addTo(map);
 
-    map.fitBounds(layerJalan.getBounds());
+    setLayerControl();
 
-});
+    zoomGabungan();
+
+})
+.catch(err => console.log("ERROR JALAN:", err));
 
 // ===============================
 // LOAD KELURAHAN
 // ===============================
-fetch("KelurahanKarangtengah.geojson")
-.then(res => res.json())
+fetch("./kelurahankarangtengah.geojson")
+.then(res => {
+    if (!res.ok) throw new Error("kelurahankarangtengah.geojson tidak ditemukan");
+    return res.json();
+})
 .then(data => {
 
-    layerKelurahan = L.geoJSON(data,{
+    layerKelurahan = L.geoJSON(data, {
 
-        style:styleKelurahan,
+        style: styleKelurahan,
 
-        onEachFeature:function(feature, layer){
+        onEachFeature: function (feature, layer) {
 
-            var namaKel = feature.properties.NAMOBJ || "Kelurahan Karangtengah";
+            var namaKel = feature.properties.KELURAHAN;
 
             layer.bindPopup("<b>Kelurahan :</b> " + namaKel);
+
+            // ZOOM SAAT DIKLIK
+            layer.on("click", function () {
+                map.fitBounds(layer.getBounds());
+            });
         }
 
     }).addTo(map);
 
-});
+    setLayerControl();
+
+    zoomGabungan();
+
+})
+.catch(err => console.log("ERROR KELURAHAN:", err));
 
 // ===============================
 // CONTROL LAYER
 // ===============================
-setTimeout(function(){
+var controlLayer;
 
-L.control.layers(
-    { "OpenStreetMap": osm },
-    {
-        "Jalan": layerJalan,
-        "Kelurahan Karangtengah": layerKelurahan
-    },
-    { collapsed:false }
-).addTo(map);
+function setLayerControl() {
 
-},1000);
+    if (controlLayer) {
+        map.removeControl(controlLayer);
+    }
+
+    var baseMaps = {
+        "OpenStreetMap": osm
+    };
+
+    var overlayMaps = {};
+
+    if (layerJalan) overlayMaps["Jalan"] = layerJalan;
+    if (layerKelurahan) overlayMaps["Kelurahan"] = layerKelurahan;
+
+    controlLayer = L.control.layers(baseMaps, overlayMaps, {
+        collapsed: false
+    }).addTo(map);
+}
 
 // ===============================
-// TUTUP PANEL
+// ZOOM KE SEMUA LAYER
 // ===============================
-function closePanel(){
+function zoomGabungan() {
+
+    var group = [];
+
+    if (layerJalan) group.push(layerJalan);
+    if (layerKelurahan) group.push(layerKelurahan);
+
+    if (group.length > 0) {
+        var gabung = L.featureGroup(group);
+        map.fitBounds(gabung.getBounds());
+    }
+}
+
+// ===============================
+// TUTUP PANEL DETAIL
+// ===============================
+function closePanel() {
 
     document.getElementById("info-panel").classList.add("hidden");
 
-    if(window.layerAktif){
-        window.layerAktif.bindTooltip(window.namaTooltip,{
-            permanent:true,
-            direction:"center",
-            className:"label-jalan"
+    if (window.layerAktif) {
+        window.layerAktif.bindTooltip(window.namaTooltip, {
+            permanent: true,
+            direction: "center",
+            className: "label-jalan"
         });
     }
 }
